5f16534dadd291f01980956762ee2718f7b7ceb7
weic6 <wei.rarr@gmail.com>
2025-06-10 04:05:24 -0500

    [fix] update default date time format and extend message converters with custom ObjectMapper

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -1,10 +1,16 @@
 package com.sky.config;
 
+import com.sky.json.JacksonObjectMapper;
 import com.sky.interceptor.JwtTokenAdminInterceptor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -66,5 +72,21 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         log.info("开始设置静态资源映射...");
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+
+    /**
+     * 扩展Spring MVC框架的消息转换器
+     * @param converters
+     */
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+         log.info("扩展消息转换器...");
+        // 创建消息转换器对象
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+
+        // 需要为消息转换器设置对象转换器，它可以将Java对象序列化为json数据
+        converter.setObjectMapper(new JacksonObjectMapper());
+        // 将自己的消息转换器追加到mvc框架的转换器集合中，0表示优先级最高
+        converters.add(0, converter);
     }
 }
